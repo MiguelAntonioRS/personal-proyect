0cd46cacc4f9a48d001e220731cf872dddfc2a6e
MiguelAntonioRS <rojassucarinomiguelantonio@gmail.com>
2025-06-11 12:33:03 -0400

    test

--- src/proyect/Person.java
+++ src/proyect/Person.java
@@ -4,6 +4,7 @@ public class Person {
     private String firstName;
     private String lastName;
     private String ci;
+    // testing
    
     public Person(String name, String lastName, String ci) {
         this.firstName = name;
